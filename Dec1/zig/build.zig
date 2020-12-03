9ee729f74b29cb12480caaf2311687893e387cfc
Olof Düsterdieck <olof.dusterdieck@ericsson.com>
2020-12-03 22:23:12 +0100

    Corrected exe names in the zig build files
    Change-Id: Ic4c94074fb23994fc61453bbdcde959d5e3d8788

--- Dec1/zig/build.zig
+++ Dec1/zig/build.zig
@@ -2,7 +2,7 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("Dec2", "src/main.zig");
+    const exe = b.addExecutable("Dec1", "src/main.zig");
     exe.setBuildMode(mode);
     exe.install();
 
